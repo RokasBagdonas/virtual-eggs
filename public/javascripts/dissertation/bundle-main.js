6951963c54230309f1fa9b6e293e4d1a00b9822a
Rokas101 <rokius.bagdonas@gmail.com>
2020-04-13 17:47:19 +0100

    layout scaffold done

--- public/javascripts/dissertation/bundle-main.js
+++ public/javascripts/dissertation/bundle-main.js
@@ -3866,10 +3866,10 @@ function render() {
 
 function onWindowResize() {
 
-    camera.aspect = container.clientWidth / container.clientHeight;
-    // update the camera's frustum
-    camera.updateProjectionMatrix();
-    renderer.setSize( container.clientWidth, container.clientHeight );
+    // camera.aspect = container.clientWidth / container.clientHeight;
+    // // update the camera's frustum
+    // camera.updateProjectionMatrix();
+    // renderer.setSize( container.clientWidth, container.clientHeight );
 }
 
 window.addEventListener( 'resize', onWindowResize );
